89d54c23a0dec91df27e537e894cc1dcdd90d093
Sritejaswini123 <sritejaswini1234@gmail.com>
2024-02-20 15:00:43 +0530

    CONGRATSCARDPROJECT

---
+++ Congratscardproject/src/App.jsx
@@ -0,0 +1,15 @@
+import Component2 from './Newcomponent/Component2'
+import './App.css'
+
+const App=() =>{
+
+  return (
+  <div> 
+    <Component2/>
+  </div>
+    
+     
+  )
+}
+
+export default App
