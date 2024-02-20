f34147666650207fd3397253fe484c4121039c41
Sritejaswini123 <sritejaswini1234@gmail.com>
2024-02-20 16:09:52 +0530

    BACKGROUND

--- Congratscardproject/src/Newcomponent/Component2/index.jsx
+++ Congratscardproject/src/Newcomponent/Component2/index.jsx
@@ -3,12 +3,11 @@ const Component2 = ()=>{
     return(
         <div className = "component2-container">
              <h1 style={{  fontFamily: 'Roboto' }}>CONGRATULATIONS</h1>
-             <div className = "component2- container2">
         <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"  className="logo" />
         <h2 style={{  fontFamily: 'Roboto' }}>Vishnu Institute of computer education and technology, bhimavaram</h2>
         <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"  className="logo" />
         </div>
-        </div>
+        
     )
 }
 export default Component2;
